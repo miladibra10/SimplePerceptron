8f996d44eb10107a50f2afefd4c546f8785acf76
Milad <miladibra10@gmail.com>
2019-03-16 01:41:26 +0330

    gitignore added and perceptron file declared

---
+++ perceptron.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Perceptron:
+    """
+    Implementation of perceptron with gradient decent algorithm for learning and updating weights
+    """
+    def __init__(self, input_number, epochs=100, learning_rate=0.01):
+        pass
+
+    def predict(self, inputs):
+        pass
+
+    def train(self, training_inputs, labels):
+        pass
+
