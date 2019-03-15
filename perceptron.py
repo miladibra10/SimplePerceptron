6a6b17248e760da5c98266abbea8694ad5be9bdb
Milad <miladibra10@gmail.com>
2019-03-16 01:48:34 +0330

    prediction function implemented

--- perceptron.py
+++ perceptron.py
@@ -6,10 +6,18 @@ class Perceptron:
     Implementation of perceptron with gradient decent algorithm for learning and updating weights
     """
     def __init__(self, input_number, epochs=100, learning_rate=0.01):
-        pass
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.weights = np.zeros(input_number + 1)
 
     def predict(self, inputs):
-        pass
+        # weight[0] is bias
+        sum = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        if sum > 0:
+            activation = 1
+        else:
+            activation = 0
+        return activation
 
     def train(self, training_inputs, labels):
         pass
